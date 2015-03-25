94a5daa7d19f35b80fe19fe4dffc8982d8e0d9e2
Keithg488 <Keithg488@gmail.com>
2015-03-25 19:24:07 -0400

    Keiths added

--- app/src/main/java/sjcnyapps/simon/MainActivity.java
+++ app/src/main/java/sjcnyapps/simon/MainActivity.java
@@ -5,15 +5,29 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainActivity extends ActionBarActivity {
+
+
+    List<Integer> SEQUENCE;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intialize();
+        StartRound();
+
     }
 
+    private void Intialize() {
+        SEQUENCE =  new ArrayList<Integer>();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
